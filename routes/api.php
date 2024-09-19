b3ccd56fcf28ab8415cdd6a85af3a80ce74b9d7b
YairMedina <114711485+YairMedina@users.noreply.github.com>
2024-09-18 23:15:49 -0600

    Seguridad en las Apis

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -28,28 +31,38 @@ Route::get('model', function () {
 });
 
 
-Route::get('productos/{id}', [App\Http\Controllers\ApiController::class, 'show']);
-Route::post('productos', [App\Http\Controllers\ApiController::class, 'store']);
-Route::put('productos/{id}', [App\Http\Controllers\ApiController::class, 'update']);
-Route::delete('productos/{id}', [App\Http\Controllers\ApiController::class, 'delete']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+        
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('vehicles/{id}', [App\Http\Controllers\VehicleController::class, 'show']);
+    Route::post('vehicles', [App\Http\Controllers\VehicleController::class, 'store']);
+    Route::put('vehicles/{id}', [App\Http\Controllers\VehicleController::class, 'update']);
+    Route::delete('vehicles/{id}', [App\Http\Controllers\VehicleController::class, 'delete']);
+
+    Route::get('clients/{id}', [App\Http\Controllers\ClientController::class, 'show']);
+    Route::post('clients', [App\Http\Controllers\ClientController::class, 'store']);
+    Route::put('clients/{id}', [App\Http\Controllers\ClientController::class, 'update']);
+    Route::delete('clients/{id}', [App\Http\Controllers\ClientController::class, 'delete']);
+
+    Route::get('models/{id}', [App\Http\Controllers\ModelController::class, 'show']);
+    Route::post('models', [App\Http\Controllers\ModelController::class, 'store']);
+    Route::put('models/{id}', [App\Http\Controllers\ModelController::class, 'update']);
+    Route::delete('models/{id}', [App\Http\Controllers\ModelController::class, 'delete']);
 
 
-Route::get('vehicles/{id}', [App\Http\Controllers\VehicleController::class, 'show']);
-Route::post('vehicles', [App\Http\Controllers\VehicleController::class, 'store']);
-Route::put('vehicles/{id}', [App\Http\Controllers\VehicleController::class, 'update']);
-Route::delete('vehicles/{id}', [App\Http\Controllers\VehicleController::class, 'delete']);
-
-Route::get('clients/{id}', [App\Http\Controllers\ClientController::class, 'show']);
-Route::post('clients', [App\Http\Controllers\ClientController::class, 'store']);
-Route::put('clients/{id}', [App\Http\Controllers\ClientController::class, 'update']);
-Route::delete('clients/{id}', [App\Http\Controllers\ClientController::class, 'delete']);
-
-Route::get('models/{id}', [App\Http\Controllers\ModelController::class, 'show']);
-Route::post('models', [App\Http\Controllers\ModelController::class, 'store']);
-Route::put('models/{id}', [App\Http\Controllers\ModelController::class, 'update']);
-Route::delete('models/{id}', [App\Http\Controllers\ModelController::class, 'delete']);
-
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
